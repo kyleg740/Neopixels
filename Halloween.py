9aa4bacb57ce373e5c29122b5bfac919ca1dacea
kyleg740 <2025Gehman.Kyle@fcboe.org>
2024-10-29 13:30:04 -0400

    Update Halloween.py

--- Halloween.py
+++ Halloween.py
@@ -17,6 +17,19 @@ colors = [red, orange, yellow, green, blue, purple, white]
 
 
 def sparkle (background, foreground, delay, number):
+    """
+    Has a background color and randomly flashes a foreground individual pixel.
+
+    Args:
+        number[int] = sets the random number
+        foreground[tuple] = sets the foreground color
+        background[tuple] = sets the background color
+        delay[double] = sets the delay
+
+    Returns:
+        a + b
+    """
+    return a + b
     np.fill(background)
     np.show()
     for i in range(random.randint(1, 3)):
@@ -26,6 +39,15 @@ def sparkle (background, foreground, delay, number):
         time.sleep(delay)
         
 def chase():
+    """
+    2 pixels chase each other.
+
+    Args:
+        count[int] = zero
+
+    Returns:
+        The pixels chase each other
+    """
     count = 0
     for i in range(np.n):
         np.fill(green)
@@ -37,6 +59,15 @@ def chase():
         count = (count + 1) % 3 
         
 def lightning(color):
+    """
+    Whole strip randomly flashes.
+
+    Args:
+        color[tuple] = sets the background color
+
+    Returns:
+        The light flashes.
+    """
     np.fill((color))
     np.show()
     time.sleep(3)
@@ -50,13 +81,76 @@ def lightning(color):
         time.sleep(sleep)
         
 def fire(num, prim_color, sec_color1, sec_color2):
+    """
+    Strips lights flicker.
+
+    Args:
+        num[int] = sets a number
+        prim_color = sets the background color
+        sec_color1 = set a color
+        sec_color2 = set a second color
+
+    Returns:
+        The leds flicker 2 colors.
+    """
     for i in range (num):
         sparkle(sec_color1, prim_color, 0.1, 0.1)
         sparkle(sec_color2, sec_color1, 0.2, 0.1)
+   
+def fade_in(color, speed):
+    """
+    Strips lights fade in.
+
+    Args:
+        color = set a color
+        speed = how quicky it fades
+
+    Returns:
+        The leds fade in.
+    """
+    red_original = color[0]
+    green_original = color[1]
+    blue_original = color[2]
+    red_ratio = color[0] / 50
+    green_ratio = color[1] / 50
+    blue_ratio = color[2] / 50
+    for i in range(1, 51):
+        A = red_original + i * red_ratio
+        B = green_original + i * green_ratio
+        C = blue_original + i * blue_ratio
+        np.fill((A, B, C))
+        np.show()
+        time.sleep(speed)
+
+def fade_out(color, speed):
+    """
+    Strips lights fade out.
+
+    Args:
+        color = set a color
+        speed = how quicky it fades
+
+    Returns:
+        The leds fade out.
+    """
+    red_original = color[0]
+    green_original = color[1]
+    blue_original = color[2]
+    red_ratio = color[0] / 50
+    green_ratio = color[1] / 50
+    blue_ratio = color[2] / 50
+    for i in range(1, 51):
+        A = red_original - i * red_ratio
+        B = green_original - i * green_ratio
+        C = blue_original - i * blue_ratio
+        np.fill((A, B, C))
+        np.show()
+        time.sleep(speed)   
    
 while True:
     fire(5, green, yellow, orange)
     chase()
     sparkle(orange, purple, 0.1, 6)
     lightning(purple)
-    
+    fade_in(orange, 0.1)
+    fade_out(orange, 0.1)
